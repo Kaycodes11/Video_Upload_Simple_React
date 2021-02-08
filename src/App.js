0b43fe24cd3cf250fd124d8785c9d19a3921ae55
Pallab Kayal <57203689+Kaycodes11@users.noreply.github.com>
2021-02-08 15:37:12 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import "./styles.css";
+
+export default function App() {
+  const [uploadedVideo, setUploadedVideo] = useState([]);
+
+  const uploadVideos = (e) => {
+    const file = e.target.files[0];
+    let reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => {
+      document.getElementById("video").src = reader.result;
+    };
+    reader.onerror = (err) => console.log(err);
+  };
+  return (
+    <div className="App">
+      <h1>Video upload</h1>
+      <input type="file" accept="video/*" onChange={uploadVideos} />
+      <hr />
+      <video width="480" controls id="video">
+        <source id="src" src="video.mp4" />
+      </video>
+      <hr />
+      <button id="start">Start</button>
+    </div>
+  );
+}
